cd061c7a86f0f5fb00a8dccfe8034068f46d1684
Chan Li Qin <chan@localhost.localdomain>
2023-04-16 14:45:14 +0900

    Transfer definitions to module to tidy up code

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -3,9 +3,16 @@ The modules provides for utility functions with respect to data from the prepare
 """
 
 # Import libraries
+import glob
+
 import math
 import matplotlib.pyplot as plt
+import numpy as np
 
+import torch
+from torch.utils.data import Dataset
+
+from osgeo import gdal
 
 
 def plotArray(arr, n_channels= None, 
@@ -49,3 +56,85 @@ band_names= [
             # fig.colorbar(f_img, ax= axs[col, row])
             plt.colorbar()
     plt.show()
+
+
+class UMPDataset(Dataset):
+    """Urban Morphological Parameters - Sentinel Dataset"""
+
+    def __init__(self, ump_df, dir_path, file_ext= ".tiff", transform= None):
+        """
+        # Parameters:
+            - ump_df: DataFrame containing the geometry of each cell with its corresponding UMPs.
+            - dir_path: Directory in which all tiff files will be matched with it's corresponding cells in ump_df.
+                - No trailing slashes for dir. eg. "data/osaka"
+                - Naming convention for files: Sentinel_{'_'.join(map(str, map(int, cell.bounds)))}.tiff for cell in ump_df["geometry"]
+            - file_ext: Extension of the file eg. .tiff / .tif
+        """
+        self.ump_df = ump_df
+        self.dir_path = dir_path
+        self.file_ext = file_ext
+        # Look in both current and sub-directories
+        self.files = glob.glob(f"{self.dir_path}/**/*{self.file_ext}") + glob.glob(f"{self.dir_path}/*{self.file_ext}")
+        self.transform = transform
+
+        # Generate the max and min for each channel and each UMP
+        self.channel_max = [0 for _ in range(12)]
+        self.UMP_max = [0 for _ in range(10)]
+        # Using the roundabout way because there seems to be a bug with iterating directly
+        for entry in range(len(self)):
+            data_piece = self[entry]
+            image = data_piece[0]
+            UMPs = data_piece[1]
+            for channel in range(len(image)):
+                # The image
+                cur_max = image[channel].max()
+                if cur_max > self.channel_max[channel]:
+                    self.channel_max[channel] = cur_max
+            
+            for ump in range(len(UMPs)):
+                cur_max = UMPs[ump]
+                if cur_max > self.UMP_max[ump]:
+                    self.UMP_max[ump] = cur_max        
+    
+    def __len__(self):
+        return len(self.ump_df)
+    
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        
+        poly = self.ump_df["geometry"][idx]
+        target = f"Sentinel_{'_'.join(map(str, map(int, poly.bounds)))}.tiff"
+        # Finds and return the first match in a string
+        file_name = next((s for s in self.files if target in s), None)
+        if file_name is None:
+            raise KeyError(f"{target} not found in {self.dir_path}")
+        
+        image = gdal.Open(file_name, gdal.GA_ReadOnly).ReadAsArray().astype("f")
+
+        if image.shape != (12, 90, 90):
+            print("Non-standard image shape:", image.shape)
+            if image.shape[0] != 12:
+                raise ValueError(f"Incorrect number of channels, {image.shape[0]} channels present when 12 expected")
+        
+        umps = np.array(self.ump_df.iloc[idx][[
+            "AverageHeightArea", 
+            "AverageHeightBuilding", 
+            "AverageHeightTotalArea", 
+            "Displacement", 
+            "FrontalAreaIndex",
+            "MaximumHeight",
+            "PercentileHeight",
+            "PlanarAreaIndex",
+            "RoughnessLength",
+            "StandardDeviation",
+        ]].astype("f"))
+        
+        out = [image, umps]
+
+        if self.transform:
+            out = self.transform(out)
+        
+        return out
+        
+
